37fa931e91e9d3b3e2c48990dc3ad0978e2571f2
Andrey Lavrinovich <lavrinovitch.andreas@yandex.ru>
2023-09-13 11:45:35 +0300

    Add a recurtion function to create a range of decreasing circle

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
 from tkinter import *
+from random import randint, shuffle, randrange
 
 root = Tk()
 root.geometry('400x200')
 colors =[f'grey{i}' for i in range(91,10,-10)]
 colors = ['white'] + colors + ['black']
+
 # print(colors)
 
 canvas = Canvas(root, background='black', width=400,height=200)
 canvas.pack()
 
 
-def create_oval(canvas,x1,y1,x2,y2, **kwargs):
+
+
+def create_oval(canvas,x1,y1,x2,y2, level = 0):
     global colors
-    if 'transparent' in kwargs:
-        transparent = kwargs.pop('transparent')
-        print(transparent)
-        canvas.create_oval(x1,y1,x2,y2, fill = colors[transparent])
-    # elif kwargs['transparent'] not in range(0,10):
-    #     raise IndexError('Слишком большое значение прозрачности')
+    if level == 10:
+        canvas.create_oval(x1,y1,x2,y2, fill = colors[level])
+        return 1
     else:
-        canvas.create_oval(x1,y1,x2,y2, fill = colors[0])
+        canvas.create_oval(x1,y1,x2,y2, fill = colors[level])
+        x_rand = randrange(-10,10)
+        y_rand = randrange(5,15)
+        x1,y1,x2,y2 = x1-x_rand,y1-y_rand,x2-x_rand,y2-y_rand
+        return create_oval(canvas,x1,y1,x2,y2, level=level+1)
+    
+        
+create_oval(canvas,180,180,190,190)
+create_oval(canvas,180,180,190,190)
+create_oval(canvas,180,180,190,190)
 
-
-create_oval(canvas,180,180,190,190,transparent = 10)
+# for i in range(11):
+#     create_oval(canvas,180,180 - i *10,190,190 - i * 10,transparent = i)
 root.mainloop()
