f7cf12123c266b027cdbb6dba80e0050c9257db8
Andrey Lavrinovich <lavrinovitch.andreas@yandex.ru>
2023-09-14 11:49:11 +0300

    End with creating the particles. Made random size

--- main.py
+++ main.py
@@ -12,66 +12,51 @@ colors = ['white'] + colors + ['black']
 canvas = Canvas(root, background='black', width=400,height=200)
 canvas.pack()
 
+def callback(event):  
+    print( event.x, event.y)
+
+canvas.bind("<Button-1>", callback)
+
+
 class Oval():
     colors = colors
     def __init__(self, canvas, x1,y1,x2,y2) -> None:
-        self.x_plus = randrange(-5,5)
-        self.y_plus = randrange(-5,5)
-        self.x1 = x1 + self.x_plus
-        self.y1 = y1+ self.y_plus
+        self.x_plus = randrange(0,2)
+        self.x1 = x1 - self.x_plus
+        self.y1 = y1 - self.x_plus
         self.x2 = x2 + self.x_plus
-        self.y2= y2+ self.y_plus
-        
+        self.y2= y2 + self.x_plus
+        print(self.x1,self.y1,self.x2,self.y2)
         self.level = 0
         self.canvas = canvas
         self.x_sum, self.y_sum = 0,0
         self.shape = canvas.create_oval(self.x1,self.y1,self.x2,self.y2, fill = Oval.colors[self.level])
-        
-        
-        self.canvas.after(randrange(100,200), self.move)
+        self.canvas.after(randrange(0,1000), self.move)
 
 
     def change_color(self,):
         self.canvas.itemconfig(self.shape, fill = colors[self.level])
         self.level += 1
         
-        
-
     def move(self):
         x_rand = randrange(-12, 12)
         y_rand = randrange(8,20)
-        # print(self.level)
-        
         if self.level == 11:
             self.level = 0
             self.canvas.move(self.shape, -self.x_sum, self.y_sum)
-            # print(self.x_sum, self.y_sum)
             self.x_sum, self.y_sum = 0,0
         else:
             self.x_sum+=x_rand
             self.y_sum+=y_rand
             self.canvas.move(self.shape, x_rand, - y_rand)
         self.change_color()
-        self.canvas.after(100, self.move)
+        self.canvas.after(75, self.move)
 
     
-time_rand = random()
+
+for i in range(20):
+    a = Oval(canvas=canvas, x1 = 180,y1=160,x2=190,y2=170)
 
 
-a = Oval(canvas=canvas, x1 = 180,y1=180,x2=195,y2=195)
-
-a = Oval(canvas=canvas, x1 = 180,y1=180,x2=195,y2=195)
-a = Oval(canvas=canvas, x1 = 180,y1=180,x2=195,y2=195)
-a = Oval(canvas=canvas, x1 = 180,y1=180,x2=195,y2=195)
-a = Oval(canvas=canvas, x1 = 180,y1=180,x2=195,y2=195)
-a = Oval(canvas=canvas, x1 = 180,y1=180,x2=195,y2=195)
-a = Oval(canvas=canvas, x1 = 180,y1=180,x2=195,y2=195)
-a = Oval(canvas=canvas, x1 = 180,y1=180,x2=195,y2=195)
-a = Oval(canvas=canvas, x1 = 180,y1=180,x2=195,y2=195)
-a = Oval(canvas=canvas, x1 = 180,y1=180,x2=195,y2=195)
-a = Oval(canvas=canvas, x1 = 180,y1=180,x2=195,y2=195)
-a = Oval(canvas=canvas, x1 = 180,y1=180,x2=195,y2=195)
-
-# root.after(100, a.)
 
 root.mainloop()
